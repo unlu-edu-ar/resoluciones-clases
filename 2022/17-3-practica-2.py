b3c288b6ce4add8f639658380a13b8d4bd8872c7
agrup <agustinrodriguez206@gmail.com>
2022-03-20 18:39:56 -0300

    add 2022

---
+++ 2022/17-3-practica-2.py
@@ -0,0 +1,29 @@
+# Tomando como base el enunciado del ejercicio anterior, 
+# modifique el programa para que ahora los valores de las 
+# variables sean ingresadas por el usuario en lugar de ser un valor hardcodeado (arbitrario). 
+# El programa debe seguir mostrando el resultado de multiplicar ambos valores
+
+# numero1 = int(input ('ingrese valor'))
+
+
+numero1 = int(input('ingrese valor para numero 1: '))
+numero2 = int(input('ingrese valor para numero 2: '))
+
+# print(numero1)
+# print(numero2)
+
+resultado = numero1 * numero2
+
+print("el resaltado es " , resultado)
+
+
+# numero1 = int()
+# input(numero1)
+
+# numero2 = 27
+
+
+
+# resultado = int(numero1) + numero2
+
+# print("el resaltado es " , resultado)
---
+++ 2022/17-3-practuca.py
@@ -0,0 +1,12 @@
+# Codifique un programa en Python que almacene dos números enteros; asigne dos valores cualesquiera a dichas variables. 
+# Finalmente, el programa debe mostrar en pantalla el resultado de multiplicar ambos valores.
+
+numero1 = '50'
+
+numero2 = 27
+
+resultado = int(numero1) + numero2
+
+print("el resaltado es " , resultado)
+
+print(hola)
